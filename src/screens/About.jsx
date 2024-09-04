3fac14aea4287f3cc48b8ecc645eab7f8c7829c6
Matias Bongiovanni <matiasweschta@gmail.com>
2024-09-04 20:54:51 -0300

    style: fixed bug Mobile responsive design

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
+import Experience from '../components/Experience'
 
 const About = () => {
   return (
     <section className='bg-[#161616] h-[600px] p-5' id='About-Me'>
          <h3 className='text-3xl text-center font-bold text-[#ccc] pt-5'>About Me</h3>
          <p className='text-[#ccc] text-center pt-2 opacity-65 pb-5'>My experience, studies and certifications.</p>
-
+          <Experience />
     </section>
   )
 }
--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -3,10 +3,11 @@ import Card from '../components/Card'
 
 const Projects = () => {
   return (
-    <section className='bg-[#161616] h-[500px] p-5' id='Projects'>
+    <section className='bg-[#161616] h-[500px]sm:h-[800px] p-5' id='Projects'>
         <h3 className='text-3xl text-center font-bold text-[#ccc] pt-5'>Projects</h3>
         <p className='text-[#ccc] text-center pt-2 opacity-65 pb-5'>Featured Projects, see all projects in my <span><a href="https://github.com/matiasbongiovanni" target="_blank" rel="noreferrer noopener" className='underline'>Github</a></span></p>
         <Card />
+        <button className='bg-[#ccc] text-[#161616] p-2 font-bold rounded'>More Projects</button>
     </section>
   )
 }
