49a4df29e4192945b74955910fb6334d6c2ea758
Matias Bongiovanni <matiasweschta@gmail.com>
2024-09-05 14:10:38 -0300

    style and add multiple components

--- src/components/Alert.jsx
+++
@@ -1,7 +0,0 @@
-import { Alert } from "@material-tailwind/react";
- 
-function AlertDefault() {
-  return <Alert>Form sent correctly, you will receive a response soon</Alert>;
-}
-
-export default AlertDefault
