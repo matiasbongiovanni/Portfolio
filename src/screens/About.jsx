4c613517326f41d9b962f84f252293e04c0fac7e
Matias Bongiovanni <matiasweschta@gmail.com>
2024-10-08 20:59:39 -0300

    fix(Fix problem responsive desing):

--- src/screens/About.jsx
+++ src/screens/About.jsx
@@ -5,7 +5,7 @@ import Skills from '../components/Skills'
 const About = () => {
   return (
     <section className='bg-[#161616] sm:h-[400px] h-[650px] p-5' id='About-Me'>
-         <h3 className='text-3xl text-center font-bold text-[#ccc] pt-5'>About Me</h3>
+         <h3 className='text-3xl text-center font-bold text-[#ccc] pt-5'>Studies</h3>
          <p className='text-[#ccc] text-center pt-2 opacity-65 pb-5'>My experience, skills, studies and certifications.</p>
           <Experience />
           <Skills />
