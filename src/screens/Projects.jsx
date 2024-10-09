76f13795cf76c7eb9649f2e3393af0a401944183
Matias Bongiovanni <matiasweschta@gmail.com>
2024-10-09 14:44:24 -0300

    style(Change differents styles in sections  and fix spacings):

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -5,8 +5,8 @@ const Projects = () => {
   return (
     <section className='h-[500px]sm:h-[800px] p-5' id='Projects'>
         <h3 className='text-3xl text-center font-bold text-[#ccc] pt-5'>Projects</h3>
-        <Card />
         <p className='text-[#ccc] text-center pt-2 pb-2 opacity-65'>Featured Projects, see all projects in my <span><a href="https://github.com/matiasbongiovanni" target="_blank" rel="noreferrer noopener" className='underline'>Github</a></span></p>
+        <Card />        
     </section>
   )
 }
