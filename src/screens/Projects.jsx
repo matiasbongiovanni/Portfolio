3bd5dd72f2db34e9e0985fe8601e79cb83a71f40
Matias Bongiovanni <matiasweschta@gmail.com>
2024-10-08 21:00:28 -0300

    style(Change texts in section):

--- src/screens/Projects.jsx
+++ src/screens/Projects.jsx
@@ -5,8 +5,8 @@ const Projects = () => {
   return (
     <section className='bg-[#161616] h-[500px]sm:h-[800px] p-5' id='Projects'>
         <h3 className='text-3xl text-center font-bold text-[#ccc] pt-5'>Projects</h3>
-        <p className='text-[#ccc] text-center pt-2 pb-2 opacity-65'>Featured Projects, see all projects in my <span><a href="https://github.com/matiasbongiovanni" target="_blank" rel="noreferrer noopener" className='underline'>Github</a></span></p>
         <Card />
+        <p className='text-[#ccc] text-center pt-2 pb-2 opacity-65'>Featured Projects, see all projects in my <span><a href="https://github.com/matiasbongiovanni" target="_blank" rel="noreferrer noopener" className='underline'>Github</a></span></p>
     </section>
   )
 }
